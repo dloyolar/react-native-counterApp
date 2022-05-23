215d9ef87b28fca20ce0a842a15e9951c974b3cf
dloyolar <diego.loyola@continuumhq.com>
2022-05-23 19:42:44 -0400

    feat: init project with react native CLI

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {CounterScreen} from './src/screens/CounterScreen';
+// import {HelloWorldScreen} from './src/screens/HelloWorldScreen';
+
+export const App = () => {
+  return (
+    <>
+      {/* <HelloWorldScreen />; */}
+      <CounterScreen />
+    </>
+  );
+};
