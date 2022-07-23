fce44450ad9504f7ccb550dac877c085852a236f
dloyolar <diego.loyola@continuumhq.com>
2022-07-23 16:01:53 -0400

    feat: playing with flex and position

--- App.tsx
+++ App.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import {FlexScreen} from './src/screens/FlexScreen';
+// import { FlexScreen } from './src/screens/FlexScreen';
 // import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
 // import {DimensionsScreen} from './src/screens/DimensionsScreen';
 // import {CounterScreen} from './src/screens/CounterScreen';
 // import {HelloWorldScreen} from './src/screens/HelloWorldScreen';
 // import {PositionScreen} from './src/screens/PositionScreen';
+import {TaskScreen} from './src/screens/TaskScreen';
 
 export const App = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{flex: 1, backgroundColor: '#28425B'}}>
       {/* <HelloWorldScreen />; */}
       {/* <CounterScreen /> */}
       {/* <BoxObjectModelScreen /> */}
       {/* <DimensionsScreen /> */}
       {/* <PositionScreen /> */}
-      <FlexScreen />
+      {/* <FlexScreen /> */}
+      <TaskScreen />
     </SafeAreaView>
   );
 };
