b18d0b5341617f77c4748f5d1e7dec70d87c4149
dloyolar <diego.loyola@continuumhq.com>
2022-07-23 15:28:55 -0400

    wip: add flex screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import {FlexScreen} from './src/screens/FlexScreen';
 // import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
 // import {DimensionsScreen} from './src/screens/DimensionsScreen';
 // import {CounterScreen} from './src/screens/CounterScreen';
 // import {HelloWorldScreen} from './src/screens/HelloWorldScreen';
-import {PositionScreen} from './src/screens/PositionScreen';
+// import {PositionScreen} from './src/screens/PositionScreen';
 
 export const App = () => {
   return (
@@ -13,7 +14,8 @@ export const App = () => {
       {/* <CounterScreen /> */}
       {/* <BoxObjectModelScreen /> */}
       {/* <DimensionsScreen /> */}
-      <PositionScreen />
+      {/* <PositionScreen /> */}
+      <FlexScreen />
     </SafeAreaView>
   );
 };
