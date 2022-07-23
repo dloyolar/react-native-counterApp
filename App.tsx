b0b9d60fa7fecbcf5dbf20d9f620710c54db3404
dloyolar <diego.loyola@continuumhq.com>
2022-07-23 16:02:40 -0400

    fix: revert counter screen

--- App.tsx
+++ App.tsx
@@ -3,21 +3,21 @@ import {SafeAreaView} from 'react-native';
 // import { FlexScreen } from './src/screens/FlexScreen';
 // import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
 // import {DimensionsScreen} from './src/screens/DimensionsScreen';
-// import {CounterScreen} from './src/screens/CounterScreen';
+import {CounterScreen} from './src/screens/CounterScreen';
 // import {HelloWorldScreen} from './src/screens/HelloWorldScreen';
 // import {PositionScreen} from './src/screens/PositionScreen';
-import {TaskScreen} from './src/screens/TaskScreen';
+// import {TaskScreen} from './src/screens/TaskScreen';
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#28425B'}}>
       {/* <HelloWorldScreen />; */}
-      {/* <CounterScreen /> */}
+      <CounterScreen />
       {/* <BoxObjectModelScreen /> */}
       {/* <DimensionsScreen /> */}
       {/* <PositionScreen /> */}
       {/* <FlexScreen /> */}
-      <TaskScreen />
+      {/* <TaskScreen /> */}
     </SafeAreaView>
   );
 };
